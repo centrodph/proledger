9b3eeba2d00320769f4a3fad03c0cc55e1b1df21
GerardoMac <centrodph@gmail.com>
2018-08-04 07:42:11 -0300

    Footer

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -4,27 +4,33 @@ import { COPYRIGHT, LINK_HOME, LINK_FAQ } from '../../utils/Constants';
 
 const Footer = () => (
   <div className="component-footer">
-    <div className="d-flex">
-      <nav className="mr-auto">
-        <ul>
-          <li>
-            <Link to={LINK_HOME}>About</Link>
-          </li>
-          <li>
-            <Link to={LINK_FAQ}>Terms of Use</Link>
-          </li>
-          <li>
-            <Link to={LINK_FAQ}>Privacy Policy</Link>
-          </li>
-          <li>
-            <Link to={LINK_FAQ}>Disclamer</Link>
-          </li>
-          <li>
-            <Link to={LINK_FAQ}>Contact Us</Link>
-          </li>
-        </ul>
-      </nav>
-      <div className="copyright">{COPYRIGHT}</div>
+    <div className="container">
+      <div className="row">
+        <div className="col-sm-12">
+          <div className="d-flex">
+            <nav className="mr-auto">
+              <ul>
+                <li>
+                  <Link to={LINK_HOME}>About</Link>
+                </li>
+                <li>
+                  <Link to={LINK_FAQ}>Terms of Use</Link>
+                </li>
+                <li>
+                  <Link to={LINK_FAQ}>Privacy Policy</Link>
+                </li>
+                <li>
+                  <Link to={LINK_FAQ}>Disclamer</Link>
+                </li>
+                <li>
+                  <Link to={LINK_FAQ}>Contact Us</Link>
+                </li>
+              </ul>
+            </nav>
+            <div className="copyright">{COPYRIGHT}</div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 );
