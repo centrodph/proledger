87cefa2529d978873982f93eb174385b1618561d
GerardoMac <centrodph@gmail.com>
2018-08-04 07:44:59 -0300

    Clean buildings

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
-import homeImage from '../../assets/images/home-page-image-left.jpg';
+import homeImage from '../../assets/images/home-page-image-left-no-buildings.jpg';
 import SignInForm from '../form/SignInForm';
 import Header from '../common/Header';
 import Footer from '../common/Footer';
